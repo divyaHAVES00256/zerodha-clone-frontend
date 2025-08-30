5e03284b6ff9ae26f8e06974ab9881f481895655
Divya Singh <t69967347@gmail.com>
2025-08-30 16:47:36 +0530

    fucking done with home ig

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
 
@@ -8,26 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   //routing setup
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-    </Routes>
-    <Routes>
-      <Route path="/signup" element={<div>Hello, World!</div>} />
-    </Routes>
-    <Routes>
-      <Route path="/about" element={<div>Hello, World!</div>} />
-    </Routes>
-    <Routes>
-      <Route path="/product" element={<div>Hello, World!</div>} />
-    </Routes>
-    <Routes>
-      <Route path="/pricing" element={<div>Hello, World!</div>} />
-    </Routes>
-    <Routes>
-      <Route path="/pricing" element={<div>Hello, World!</div>} />
-    </Routes>
-  </BrowserRouter>
 
 );
 
