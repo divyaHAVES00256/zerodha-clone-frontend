883afa99dc48069def76daea7cc46a209017a567
Divya Singh <t69967347@gmail.com>
2025-08-31 17:18:49 +0530

    product component done

--- src/landing_page/product/Hero.js
+++ src/landing_page/product/Hero.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <h1>hero</h1>
+    return (
+      <div style={{ marginTop: "10rem" }}>
+        <div className="container mb-5  mt-5">
+          <div className="row p-3 text-center">
+            <p className="display-4 fs-1 fw-semibold mb-3">Zerodha Products</p>
+            <p className=" fs-5 text-muted">
+              Sleek, modern, and intuitive trading platforms
+            </p>
+            <p className=" fs-5 text-muted mt-3">
+              Check out our investment
+              <a href="#offerings" style={{textDecoration: "none"}}>  offerings →</a>
+            </p>
+          </div>
+
+          <div
+            className="row mt-4 justify-content-center"
+            style={{
+              width: "100%",
+              height: "1px",
+              backgroundColor: "#dee2e6",
+              margin: "7rem 0",
+            }}
+          ></div>
+        </div>
+      </div>
     );
 }
 
